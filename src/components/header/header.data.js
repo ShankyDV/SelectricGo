65ad06bf9080564a5547aec1a0403e950d175f20
Shanky <varundeepanshu@gmail.com>
2022-06-04 10:55:26 +0530

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    path: 'banner',
+    label: 'Home',
+  },
+  {
+    path: 'news',
+    label: 'Slots',
+  },
+  {
+    path: 'services',
+    label: 'Services',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+ 
+  {
+    path: 'testimonials',
+    label: 'Testimonials',
+  },
+];
---
+++ src/sections/services.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Box, Container, Grid } from 'theme-ui';
+import BlockTitle from 'components/block-title';
+import ServiceCard from 'components/cards/service-card';
+import serviceImage1 from 'assets/service-1.png';
+import serviceImage2 from 'assets/service-2.png';
+import serviceImage3 from 'assets/service-3.png';
+import serviceImage4 from 'assets/service-4.png';
+import serviceImage5 from 'assets/service-5.png';
+import serviceImage6 from 'assets/service-6.png';
+
+const SERVICES_DATA = [
+  {
+    image: serviceImage1,
+    text:
+      'We provide the user with a wide range of Electric Vehicle charging stations in India.',
+    heading: 'Fuel Up',
+    path: '#',
+  },
+  {
+    image: serviceImage2,
+    text:
+      'Our app is going to find you the real-time availability of the nearest charging station!',
+    heading: 'Grab a Bite ',
+    path: '#',
+  },
+  {
+    image: serviceImage3,
+    text:
+      'Use our app to schedule the charging slot at whenever you want!',
+    heading: 'Customer Friendly',
+    path: '#',
+  },
+  // {
+  //   image: serviceImage4,
+  //   text:
+  //     'Get your info tests delivered at home collect a sample from the your progress tests.',
+  //   heading: 'Marketing & advertising',
+  //   path: '#',
+  // },
+  // {
+  //   image: serviceImage5,
+  //   text:
+  //     'Get your info tests delivered at home collect a sample from the your progress tests.',
+  //   heading: 'Ultimate development',
+  //   path: '#',
+  // },
+  // {
+  //   image: serviceImage6,
+  //   text:
+  //     'Get your info tests delivered at home collect a sample from the your progress tests.',
+  //   heading: 'Online support',
+  //   path: '#',
+  // },
+];
+const Services = () => {
+  return (
+    <Box as="section" id="services" sx={styles.services}>
+      <Container>
+        <BlockTitle
+          title="HOW WE WORK?"
+        />
+        <Grid sx={styles.grid}>
+          {SERVICES_DATA.map(({ image, text, heading, path }, index) => (
+            <ServiceCard
+              image={image}
+              text={text}
+              heading={heading}
+              path={path}
+              key={index}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Services;
+
+const styles = {
+  services: {
+    pt: ['80px', null, null, null, '80px', null, '100px'],
+  },
+  grid: {
+    gridGap: '30px',
+    gridTemplateColumns: ['1fr', null, null, '1fr 1fr', null, '1fr 1fr 1fr'],
+  },
+};
